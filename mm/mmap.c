c568bc22e59e0a30546c4eeaf6ccc0228ece6cf0
Thunderoar <farhansamilme@gmail.com>
2018-03-04 02:09:06 +0800

    FROMLIST: mm: mmap: Add new /proc tunable for mmap_base ASLR.

--- mm/mmap.c
+++ mm/mmap.c
@@ -59,9 +59,15 @@
 #define arch_rebalance_pgtables(addr, len)		(addr)
 #endif
 
-#ifdef CONFIG_PROC_FS
-static unsigned int total_no_of_find_vma = 0;
-static unsigned int vma_cache_found = 0;
+#ifdef CONFIG_HAVE_ARCH_MMAP_RND_BITS
+const int mmap_rnd_bits_min = CONFIG_ARCH_MMAP_RND_BITS_MIN;
+const int mmap_rnd_bits_max = CONFIG_ARCH_MMAP_RND_BITS_MAX;
+int mmap_rnd_bits __read_mostly = CONFIG_ARCH_MMAP_RND_BITS;
+#endif
+#ifdef CONFIG_HAVE_ARCH_MMAP_RND_COMPAT_BITS
+const int mmap_rnd_compat_bits_min = CONFIG_ARCH_MMAP_RND_COMPAT_BITS_MIN;
+const int mmap_rnd_compat_bits_max = CONFIG_ARCH_MMAP_RND_COMPAT_BITS_MAX;
+int mmap_rnd_compat_bits __read_mostly = CONFIG_ARCH_MMAP_RND_COMPAT_BITS;
 #endif
 
 
